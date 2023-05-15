fd693445b3997e6b6a7be269736c132043e3e509
mahdikalhor1 <mahdikalhor@aut.ac.ir>
2023-05-15 13:27:44 +0330

    add process of signup

--- clinet/connection.py
+++ clinet/connection.py
@@ -3,8 +3,8 @@ import psycopg2
 class Connection:
 
     def __init__(self):
-        conn = psycopg2.connect(
+        self.conn = psycopg2.connect(
             database="bank_db", user='postgres', password='postgres', host='127.0.0.1', port='5432'
         )
 
-        self.cur = conn.cursor()
+        self.cur = self.conn.cursor()
--- clinet/opearations.py
+++ clinet/opearations.py
@@ -1,28 +1,30 @@
 from connection import Connection
 
 
-conn = Connection()
-cur = conn.cur
+connection = Connection()
+conn = connection.conn
+cur = connection.cur
 
-def signin():
+def signup():
 
     first_name = input("Enter Your first name:")
     last_name = input("Last name:")
     national_id = input("National ID:")
     date_of_birth = input("date of base:(yyyy-mm-dd)")
-    type_ = input("Type: (client or employee)")
+    account_type = input("Type: (client or employee)")
     interest_rate = float(input("Interest rate: (a number between 0 and 1)"))
     password = input("Password:")
 
-    username = str
-    message = str
+    username = ""
+    message = ""
 
     cur.execute("""CALL register(%s::VARCHAR(50) ,%s::VARCHAR(20),
        %s::VARCHAR(20), %s::CHAR(10), %s::DATE,
        %s::VARCHAR(8), %s::NUMERIC(2,2), %s::VARCHAR(100),
         %s::VARCHAR(20));""", [password, first_name, last_name, national_id,
-                                   date_of_birth, type_, interest_rate, message,
-                                   username])
-    print(cur.fetchone()[0])
+                                   date_of_birth, account_type, interest_rate, message,username])
 
-signin()
+    conn.commit()
+    result = cur.fetchone()
+    print(result[0] + "\nusername: "+result[1])
+
